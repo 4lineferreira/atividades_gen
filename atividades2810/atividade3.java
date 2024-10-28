9c82d1db026e8af92e8ac81b08ba7d3eb316fff4
4lineferreira <alineferreirasnsd@gmail.com>
2024-10-28 14:00:00 -0300

    atividade 3 finalizada

--- atividades2810/atividade3.java
+++ atividades2810/atividade3.java
@@ -1,9 +1,29 @@
 package atividades2810;
 
+import java.util.Scanner;
+
 public class atividade3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner ler = new Scanner(System.in);
+		
+		float salBruto, adiNoturno, hExtra, desconto;
+		
+		System.out.println("Digite o sálario bruto: ");
+		salBruto = ler.nextFloat();
+		
+		System.out.println("Digite o valor do adicional bruto: ");
+		adiNoturno = ler.nextFloat();
+		
+		System.out.println("Digite o valor das horas extras: ");
+		hExtra = ler.nextFloat();
+		
+		System.out.println("Digite o valor dos descontos: ");
+		desconto = ler.nextFloat();
+		
+		System.out.println("Salário liquido: " + (salBruto + adiNoturno + (hExtra * 5) - desconto));
+		
 
 	}
 
