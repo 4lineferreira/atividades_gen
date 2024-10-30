5691ab01e72bdbcd58ee72f541c631207c2ba0ec
4lineferreira <alineferreirasnsd@gmail.com>
2024-10-29 22:32:13 -0300

    Ajuste em atividades

--- lacoCondicional2910/atividade3.java
+++ lacoCondicional2910/atividade3.java
@@ -5,35 +5,24 @@ import java.util.Scanner;
 public class atividade3 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner ler = new Scanner(System.in);
-		
+
 		int idade;
 		String doador, resultado;
 		boolean primeiraDoacao;
-		
+
 		System.out.println("Digite o nome do doador: ");
 		doador = ler.next();
-		
+
 		System.out.println("Digite a idade do doador: ");
 		idade = ler.nextInt();
 		
-		if (idade < 18 && idade > 69) {
-			resultado = " apto ";
-		} else {
-			resultado = " não apto ";
-		}
-				
-		System.out.println("Primeira doação? (Sim ou não): ");
-		primeiraDoacao = ler.nextBoolean();
-		
-		
-		
-		
-		
-		//terminar depois não é uma prioridade
-		
-
-	}
+			if (idade < 18 || idade > 69){
+				System.out.println("Você não está apto para doação.");
+				return;
+			}else System.out.println("Primeira doação? \n[true] sim \\n[false] não ");
+			primeiraDoacao = ler.hasNext();
+	        }
 
 }
