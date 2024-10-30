a81aafda8b9fdd96cba1a1e3b93ea2587c56e34a
4lineferreira <alineferreirasnsd@gmail.com>
2024-10-30 19:55:46 -0300

    Atividade 2 de laço condicional finalizada

---
+++ LacoForWhileDoWhile/LacoWhileAtividade3.java
@@ -0,0 +1,30 @@
+package LacoForWhileDoWhile;
+
+import java.util.Scanner;
+
+public class LacoWhileAtividade3 {
+
+	public static void main(String[] args) {
+
+		int contadorMenor = 0, idade, contadorMaior = 0;
+		Boolean continuar = true;
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (continuar) {
+			System.out.println("Digite uma idade: ");
+			idade = scanner.nextInt();
+
+			if (idade < 0) {
+				continuar = false;
+				
+			} if(idade <=21) {
+				contadorMenor++;
+			} if (idade > 50) {
+				contadorMaior++;
+			}
+		} System.out.println("idade até 25: " + contadorMenor);
+		System.out.println("idade até 25: " + contadorMaior);
+	} 
+
+}
